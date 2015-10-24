b83526af88367856d2885aad3cc620d99263231b
Cristian Rios <matrixcmr@gmail.com>
2015-10-23 22:39:42 -0300

    styles fixes

--- angular-2-ts/public/common/shadow_dom.ts
+++ angular-2-ts/public/common/shadow_dom.ts
@@ -5,7 +5,7 @@ import { document } from 'angular2/src/facade/browser';
 
 // se supone que el navegador que estoy usando soporta ShadowDom.... 
 // pero no renderiza el contenido de la vista, por lo cual por el momento lo fuerzo a false
-// export const isNativeShadowDOMSupported: boolean = Boolean(document && document.body && document.body.createShadowRoot); // http://caniuse.com/#feat=shadowdom
+//export const isNativeShadowDOMSupported: boolean = Boolean(document && document.body && document.body.createShadowRoot); // http://caniuse.com/#feat=shadowdom
 export const isNativeShadowDOMSupported = false;
 
 export const shadowDomInjectables: Array<any> = !isNativeShadowDOMSupported ? [] : [
