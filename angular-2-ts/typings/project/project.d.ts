78bee213247c31ddddc5ff776c7119c2e109b828
Cristian Rios <matrixcmr@gmail.com>
2015-10-25 17:33:21 -0300

    project typings

--- angular-2-ts/typings/project/project.d.ts
+++ angular-2-ts/typings/project/project.d.ts
@@ -1,10 +1,23 @@
 declare module project {
 	
 	interface IPost {
-		PostID: number;
-		PostDate: string;
+		PostID?: number;
+		PostDate?: string;
 		PostTitle: string;
 		PostText: string;
+		PostCategory: string;
+	}
+	
+	interface IPostID {
+		PostID: number;
+	}
+	
+	interface IPostService {
+		createPost(post: IPost): Promise<IPostID>;
+		deletePost(id: number): Promise<any>;
+		getPost(id: number): Promise<IPost>;
+		getPostList(): Promise<Array<IPost>>;
+		updatePost(post: IPost): Promise<IPost>;
 	}
 	
 }
